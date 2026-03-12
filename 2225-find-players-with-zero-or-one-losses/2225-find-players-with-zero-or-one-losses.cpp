04cf8ea11e8d35e08c70aa1922ce07a7ddf8baf9
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-03-12 22:22:34 +0530

    Time: 128 ms (64.29%), Space: 172.6 MB (47.74%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -3,38 +3,38 @@ public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) { 
         int n = matches.size();
 
-        set<int> st;
-        for(int i =0; i < n; i++){
-            st.insert(matches[i][0]);
-            st.insert(matches[i][1]);
-        }
+        unordered_map<int, int> mpp1;
+          unordered_map<int, int> mpp2;
 
-        unordered_map<int, int> mpp;
-
-        vector<int> winner;
-        vector<int> defeat1;
+        vector<int> zeroMatchLost;
+        vector<int> oneMatchLost;
 
         for(int i =0; i < n; i++){
-            mpp[matches[i][1]]++;
+            mpp2[matches[i][1]]++;
         }
 
-        for(auto it: st){
-            if(mpp.find(it) == mpp.end()){
-                winner.push_back(it);
+         for(int i =0; i < n; i++){
+            mpp1[matches[i][0]]++;
+        }
+
+        for(auto &it: mpp1){
+            if(mpp2.find(it.first) == mpp2.end()){
+                zeroMatchLost.push_back(it.first);
             } 
         }
 
-        for(auto &it: mpp){
+        for(auto &it: mpp2){
             if(it.second == 1){
-                defeat1.push_back(it.first);
+                oneMatchLost.push_back(it.first);
             }
         }
 
-        sort(defeat1.begin(), defeat1.end());
+        sort(zeroMatchLost.begin(), zeroMatchLost.end());
+        sort(oneMatchLost.begin(), oneMatchLost.end());
         // vector<vector<int>> result; // No need
         // result.push_back(winner);
         // result.push_back(defeat0);
-        return {winner, defeat1};
+        return {zeroMatchLost, oneMatchLost};
 
     }
 };
