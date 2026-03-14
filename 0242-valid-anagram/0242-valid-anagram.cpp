8800a17118128a2b3417054ed41b7bcf61c9c11c
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-03-14 10:02:21 +0530

    Time: 0 ms (100%), Space: 9.6 MB (73.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n1= s.size();
-        int n2= t.size();
+        
+        vector<int> freq(26, 0);
 
-        if(n1 < n2) return false;
-        if(n1 > n2) return false;
-
-        unordered_map<char, int> mpp;
-        for(int i =0; i < n1; i++){
-            mpp[s[i]]++;
+        for(int i =0; i < s.size(); i++){
+            freq[s[i] - 'a']++;
         }
 
-        for(int i =0; i < n1; i++){
-            if(mpp.find(t[i]) != mpp.end()){
-                mpp[t[i]]--;
-                if(mpp[t[i]] <= 0) mpp.erase(t[i]);
-            } else{
-                return false;
-            }
+        for(int i =0; i < t.size(); i++){
+            freq[t[i] - 'a']--;
+        }
+
+        for(int i =0; i < freq.size(); i++){
+            if(freq[i] != 0) return false;
         }
         return true;
     }
