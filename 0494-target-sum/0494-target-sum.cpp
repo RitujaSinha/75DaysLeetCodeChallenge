3eef6613432c94fbecd526a976a1f8de454e286b
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-03-24 00:29:34 +0530

    Time: 265 ms (29.61%), Space: 62.2 MB (6.14%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-    void solve(int idx, int currSum,int &count, vector<int>& nums, int target){
+    map<pair<int, int>, int> mp;
+    int solve(int idx, int currSum,int &count, vector<int>& nums, int target){
         if(idx >= nums.size()){
             if(currSum == target){
-                count+=1;
+                return 1;
+            } else{
+                return 0;
             }
-            return;
         }
 
         //if(currSum > target) return;
+        if(mp.find({idx, currSum}) != mp.end()){
+            return mp[{idx, currSum}];
+        }
 
-        solve(idx+1,currSum-nums[idx], count, nums, target);
 
-        solve(idx+1,currSum+ nums[idx], count, nums, target);
+        long long plus = solve(idx+1,currSum-nums[idx], count, nums, target);
 
-        return;
+        long long minus = solve(idx+1,currSum+ nums[idx], count, nums, target);
+
+        return mp[{idx, currSum}] = plus+minus;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
+        mp.clear();
 
         int count =0;
-
-        solve(0,0 ,count, nums, target);
-
-        return count;
+        
+        return solve(0,0 ,count, nums, target);
     }
 };
