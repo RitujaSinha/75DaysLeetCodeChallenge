f1cb15b854680f671e3781f8046fff03b1ce1c03
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-03-26 18:26:10 +0530

    Time: 0 ms (100%), Space: 7.9 MB (52.41%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int t[46];
+    int solve(int n){
+        if(n==0){
+            return 1;
+        }
+        if(n < 0) return 0;
+
+        if(t[n] != -1) return t[n];
+
+        int oneSteps = solve(n-1);
+        int twoSteps = solve(n-2);
+
+        return t[n] = oneSteps + twoSteps;
+    }
+
+    int climbStairs(int n) {
+        memset(t, -1, sizeof(t));
+        return solve(n);
+    }
+};
