b1e6e75ff084dcc9ba0497678270a5c6f517915e
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-03-26 10:03:16 +0530

    Time: 0 ms (100%), Space: 9.1 MB (54.85%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -3,45 +3,30 @@ public:
     long long countCommas(long long n) {
         if(n < 1000) return 0;
 
-
-        long long commas =0;
-
         string s = to_string(n);
 
-        if(s.size() >=4 && s.size() <= 6){
-            long long number = n - 999;
-            commas += (number * 1);
-        } else if(s.size() > 6 && s.size() <= 9){
-            commas += (999999-999)*1;
-            long long number = n - 999999;
-
-            commas += (number * 2);
+        long long commas =0;
+        if(s.size() >= 4 && s.size() <= 6){
+            commas += (n-999) * 1;
+        } else if(s.size() >= 7 && s.size() <= 9){
+            commas += 1 * (999999-999);
+            commas += 2 * (n-999999);
+        } else if(s.size() >= 10 && s.size() <= 12){
+            commas += 1 * (999999-999);
+            commas += 2 * (999999999-999999);
+            commas += 3 * (n-999999999);
+        } else if(s.size() >= 13 && s.size() <= 15){
+            commas += 1 * (999999-999);
+            commas += 2 * (999999999-999999);
+            commas += 3 * (999999999999-999999999);
+            commas += 4 * (n-999999999999);
+        } else if(s.size() >= 15 && s.size() <= 18){
+            commas += 1 * (999999-999);
+            commas += 2 * (999999999-999999);
+            commas += 3 * (999999999999-999999999);
+            commas += 4 * (999999999999999-999999999999);
+            commas += 5 * (n-999999999999999);
         }
-        else if(s.size() >9 && s.size() <= 12){
-             commas += (999999-999)*1;
-            commas += (999999999 - 999999) * 2;
-            long long number = n - 999999999;
-
-            commas += (number * 3);
-        }
-        else if(s.size() > 12 && s.size() <= 15){
-              commas += (999999-999)*1;
-            commas += (999999999 - 999999) * 2;
-            commas+= (999999999999-999999999) * 3;
-            long long number = n - 999999999999;
-
-            commas += (number * 4);
-        } else if(s.size() >= 16 && s.size() <= 18){
-            commas += (999999 - 999) * 1;
-            commas += (999999999 - 999999) * 2;
-            commas += (999999999999 - 999999999) * 3;
-            commas += (999999999999999 - 999999999999) * 4;
-            long long number = n - 999999999999999;
-            commas += number * 5;
-        }
-
-
-        
         return commas;
     }
 };
