e6befcd4fb7d9cc3e5bc50066f7e821d1b2b407a
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-03-27 10:15:34 +0530

    Time: 0 ms (100%), Space: 15.1 MB (42.01%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -3,16 +3,15 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
 
-        vector<vector<int>> result = matrix;
+        vector<vector<int>> t = matrix;
 
-        for(int row = n-2; row >= 0; row--){
-            for(int col =0; col < n; col++){
-                result[row][col] = matrix[row][col] + 
-                std::min({result[row+1][std::max(col-1, 0)], 
-                result[row+1][col], 
-                result[row+1][std::min(col+1,(int)result[row+1].size()-1)]});
+        for(int row = n-2; row >=0; row--){
+            for(int col = 0 ;col < n; col++){
+                t[row][col] = t[row][col] + min({t[row+1][max(col-1, 0)],
+                t[row+1][col], 
+                t[row+1][min(col+1, (int)t[row+1].size()-1)]});
             }
         }
-        return *min_element(result[0].begin(), result[0].end());
+        return *min_element(t[0].begin(), t[0].end());
     }
 };
