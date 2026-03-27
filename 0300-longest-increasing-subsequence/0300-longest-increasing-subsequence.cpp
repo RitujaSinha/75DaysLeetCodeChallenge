c9acb8a6eb159d7320f537e3e201aeb65d0ec305
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-03-27 18:41:56 +0530

    Time: 0 ms (100%), Space: 14 MB (86.53%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -3,17 +3,17 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> t(n, 1);
+        vector<int> sorted;
 
-        int maxL =1;
-        for(int i =0; i< n; i++){
-            for(int j =0; j < i; j++){
-                if(nums[i] > nums[j]){
-                    t[i] = max(t[i], t[j]+1);
-                    maxL = max(maxL, t[i]);
-                }
+        for(int i =0; i < n; i++){
+            auto it= lower_bound(sorted.begin(), sorted.end(), nums[i]);
+
+            if(it == sorted.end()){
+                sorted.push_back(nums[i]);
+            } else{
+                *it = nums[i];
             }
         }
-        return maxL;
+        return sorted.size();
     }
 };
