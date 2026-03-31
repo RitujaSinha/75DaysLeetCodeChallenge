34d29ea3f575951776570deaa854d632dac2bfbf
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-04-01 01:25:22 +0530

    Time: 70 ms (59.55%), Space: 75.9 MB (44.41%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int t[1001][1001];
-    int solve(string &s1, string &s2, int i, int j){
-        if(i >= s1.size() || j >= s2.size()) return 0;
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
 
-        if(t[i][j] != -1) return t[i][j];
+        vector<vector<int>> t(n, vector<int> (n));
+        //t[i][j] = LPS in s[i..j]
 
-        if(s1[i] == s2[j]){
-            return t[i][j] = 1 + solve(s1, s2, i+1, j+1);
+        for(int i =0; i < n; i++){
+            t[i][i] = 1; //1 length is always a plaindrome
         }
 
-        int a = solve(s1, s2, i+1, j);
-        int b = solve(s1, s2, i, j+1);
+        for(int L =2; L<=n; L++){
+            for(int i =0; i < n-L+1; i++){
+                int j = i+L-1;
 
-        return t[i][j] = max(a, b);
-    }
-    int longestPalindromeSubseq(string s) {
-        
-        string s1 =s;
-        string s2 =s1;
-        reverse(s1.begin(), s1.end());
-
-        memset(t, -1, sizeof(t));
-        return solve(s1, s2, 0, 0);
+                if(s[i] == s[j]){
+                    t[i][j] = 2 + t[i+1][j-1];
+                } else{
+                    t[i][j] = max(t[i][j-1], t[i+1][j]);    
+                }
+            }
+        }
+        return t[0][n-1]; //Pure string ka LPS
     }
 };
