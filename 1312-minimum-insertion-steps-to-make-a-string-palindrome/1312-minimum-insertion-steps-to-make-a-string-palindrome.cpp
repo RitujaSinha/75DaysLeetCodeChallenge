b5b1cecdb6ed7773c551f838005b3212be0c95bc
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-04-01 20:00:34 +0530

    Time: 19 ms (88.97%), Space: 10.1 MB (81.65%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int t[501][501];
-    int solve(string &s1, string &s2, int i, int j){
-        if(i >= s1.size() || j >= s2.size()) return 0;
+    int solve(string &s, int i, int j){
+        if(i >= j) return 0;
 
         if(t[i][j] != -1) return t[i][j];
 
-        if(s1[i] == s2[j]){
-            return t[i][j]=  1 + solve(s1, s2, i+1, j+1);
+        if(s[i] == s[j]){   
+            return t[i][j] = solve(s, i+1, j-1);
         }
 
-        return t[i][j] = max(solve(s1, s2, i+1, j), solve(s1, s2, i, j+1));
+        return t[i][j]=  1+ min(solve(s, i+1, j), solve(s, i, j-1));
     }
     int minInsertions(string s) {
-        int n = s.size();
+        int n =s.size();
 
-        string s1 = s;
-        string s2 = s1;
         memset(t, -1, sizeof(t));
 
-        reverse(s2.begin(), s2.end());
-
-        return n- solve(s1, s2, 0, 0);
+        return solve(s, 0, n-1);
     }
 };
