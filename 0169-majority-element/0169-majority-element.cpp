45e7e6905d7fdb45c7a4d9fb9248b31505b2fe85
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-04-03 19:02:00 +0530

    Time: 1439 ms (6.24%), Space: 28 MB (86.89%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,13 +4,13 @@ public:
         int n = nums.size();
 
         for(int i =0; i < n; i++){
-            int cnt =0;
-            for(int j =0; j < n; j++){
-                if(nums[i] == nums[j]) cnt++;
+            int freq =0;
+            for(int j =i; j < n; j++){
+                if(nums[i] == nums[j]) freq++;
 
-                if(cnt > n/2) return nums[i];
+                if(freq > n/2) return nums[i];
             }
         }
-        return -1;
+        return 0;
     }
 };
