a34f0fc9d347e256f0f882489031c0ece82646c9
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-04-06 15:37:56 +0530

    Time: 588 ms (5.04%), Space: 71.8 MB (51.82%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,13 +4,33 @@ public:
         int n =nums.size();
 
         int currSum =0, maxSum =INT_MIN;
+
+        int startIdx = -1, endIdx =-1;
+        int start =-1;
         for(int i =0; i< n; i++){
+            if(currSum ==0) start = i;
+
             currSum += nums[i];
 
-            maxSum = max(maxSum, currSum);
+            if(maxSum < currSum){
+                maxSum = currSum;
+                startIdx = start;
+                endIdx = i;
+            }
 
             if(currSum < 0) currSum =0;
         }
+
+
+
+         // Print subarray
+         //folow up
+        cout << "Subarray: ";
+        for(int i = startIdx; i <= endIdx; i++){
+            cout << nums[i] << " ";
+        }
+        cout << endl;
+
         return maxSum;
     }
 };
