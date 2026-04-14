055e1695582b952859d816e3c4f636ae3b7d0836
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-04-14 21:42:28 +0530

    Time: 0 ms (100%), Space: 11.8 MB (52.37%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,30 +1,45 @@
 class Solution {
 public:
-    int t[101][101];
-    int solve(vector<vector<int>>& obstacleGrid, int m, int n, int i, int j){
-        if(i == m-1 && j == n-1) return 1;
-
-        if(i >= m || j >= n || obstacleGrid[i][j] == 1){
-            return 0;
-        }
-
-        if(t[i][j] != -1) return t[i][j];
-
-        int right = solve(obstacleGrid, m, n, i, j+1);
-        int down = solve(obstacleGrid, m, n, i+1, j);
-
-        return t[i][j] = right+down;
-    }
-
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
 
-        if(obstacleGrid[m-1][n-1] == 1) return 0;
-        if(obstacleGrid[0][0] == 1) return 0;
+        vector<vector<int>> t(m, vector<int> (n, 1));
 
-        memset(t, -1, sizeof(t));
+        //fill first row
+        for(int col = 0; col < n; col++){
+            if(col >= 1 && obstacleGrid[0][col-1] == 1){
+                t[0][col] = 0;
+                obstacleGrid[0][col] =1;
+            } else if(obstacleGrid[0][col]==1){
+                t[0][col] = 0;
+            } else{
+                t[0][col]=1;
+            }
+        }
 
-        return solve(obstacleGrid,m, n, 0, 0);
+        //fill first col
+        for(int row = 1; row < m; row++){
+           if(row >= 0 && obstacleGrid[row-1][0] == 1){
+                t[row][0] = 0;
+                obstacleGrid[row][0] = 1;
+            }  else if(obstacleGrid[row][0] == 1){
+                t[row][0] =0;
+            } else{
+                t[row][0] = 1;
+            }
+        }
+
+        //fill remaining
+        for(int i =1; i < m; i++){
+            for(int j =1; j < n; j++){
+                if(obstacleGrid[i][j] == 1){
+                    t[i][j] = 0;
+                } else{
+                    t[i][j] = t[i-1][j] + t[i][j-1];
+                }
+            }
+        }
+        return t[m-1][n-1];
     }
 };
