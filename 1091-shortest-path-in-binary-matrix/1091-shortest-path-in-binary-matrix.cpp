163e1af5a4d18e67372b1a0d6e3b27bc4b326095
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-04-18 01:31:47 +0530

    Time: 23 ms (36.6%), Space: 26.2 MB (39.25%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -5,41 +5,36 @@ public:
 
         vector<vector<int>> vis(n, vector<int> (n, 0));
 
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+        if(grid[0][0] == 1 || grid[n-1][n-1]) return -1;
 
-        queue<pair<int, int>> q;
-        q.push({0, 0});
+        queue<pair<pair<int, int>,int>> q;
+        q.push({{0, 0},1});  
         vis[0][0] = 1;
 
         int delrow[] = {0,1, 1, 1, 0, -1, -1, -1};
         int delcol[] = {-1, -1, 0, 1, 1, 1, 0, -1};
 
-        int len =1;
         while(!q.empty()){
-            int size = q.size(); 
-
-            for(int k =0; k < size; k++){//process level
-            int r = q.front().first;
-            int c = q.front().second;
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            
+            int dist = q.front().second;
             q.pop();
 
-            if(r== n-1 && c == n-1) return len;
-
-            for(int i =0; i< 8; i++){
+            if(r == n-1 && c == n-1) return dist;
+            
+            for(int i =0; i < 8; i++){
                 int nrow = r + delrow[i];
                 int ncol = c + delcol[i];
 
-                if(nrow>= 0 && nrow < n && ncol < n && ncol >=0 && grid[nrow][ncol] == 0 
-                && vis[nrow][ncol] == 0){
-                    // len++;
+                if(nrow >= 0 && nrow < n && ncol >=0 && ncol < n && grid[nrow][ncol] == 0 
+                && !vis[nrow][ncol]){
                     vis[nrow][ncol] =1;
-                    q.push({nrow, ncol});
+                    q.push({{nrow, ncol}, dist+1});
                 }
             }
-            }
-            len++;  // increase after finishing level
-
-        }
+            
+        }  
         return -1;
     }
 };
