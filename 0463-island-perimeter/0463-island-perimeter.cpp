f118c510bff0ce06a24bf9fd6e754cea1f03ef57
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-04-17 21:37:30 +0530

    Time: 22 ms (13.39%), Space: 108.7 MB (7.02%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+     int bfs(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& vis){
+        int m = grid.size();
+        int n = grid[0].size();
+
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        vis[row][col] = 1;
+        int perimeter  =0;
+
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            
+            q.pop();
+
+
+            int delrow[] = {-1, 0, 1, 0};
+            int delcol[] = {0, 1, 0, -1}; 
+
+            for(int i =0; i < 4; i++){
+                
+                int nrow = r + delrow[i];
+                int ncol = c + delcol[i];
+
+                if(nrow  < 0 || nrow >= m || ncol >= n || ncol < 0 || grid[nrow][ncol] == 0){
+                    perimeter++;
+                }
+                else if(!vis[nrow][ncol]){
+                    q.push({nrow, ncol});
+                    vis[nrow][ncol]=1;
+                }
+            }
+        }
+        return perimeter;
+    }
+
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> vis(m, vector<int> (n, 0));
+
+        for(int i =0; i < m; i++){
+            for(int j =0; j < n; j++){
+                if(grid[i][j] == 1 && !vis[i][j]){
+                     return bfs(i, j, grid, vis);
+                }
+            }
+        }
+        return 0;
+    }
+};
