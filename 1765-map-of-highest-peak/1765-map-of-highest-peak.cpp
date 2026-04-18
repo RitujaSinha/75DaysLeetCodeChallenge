a0571ee76fef4d1d7b2541e9a83482051b98e761
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-04-18 11:38:05 +0530

    Time: 100 ms (15.43%), Space: 128.1 MB (17.43%) - LeetHub

---
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& mat) {
+         int m = mat.size();
+        int n = mat[0].size();
+        
+        vector<vector<int>> dist(m, vector<int> (n, 0));
+        vector<vector<int>> vis(m, vector<int> (n, 0));
+
+        queue<pair<pair<int, int>, int>> q;
+
+        for(int i =0; i < m; i++){
+            for(int j =0; j < n; j++){
+                if(mat[i][j] == 1){
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 1;
+                }
+            }
+        }
+
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+
+            int dis = q.front().second;
+
+            dist[r][c] = dis;
+
+            q.pop();
+
+            int delrow[] = {-1, 0, 1, 0};
+            int delcol[] = {0, 1, 0, -1};
+
+            for(int i =0; i <4; i++){
+
+                int nrow = r + delrow[i];
+                int ncol = c + delcol[i];
+
+                if(nrow >=0 && nrow < m && ncol >=0 && ncol < n
+                && vis[nrow][ncol] == 0){
+                    q.push({{nrow, ncol}, dis+1});
+                    vis[nrow][ncol] =1;
+                }
+            }
+        }
+        return dist;
+    }
+};
