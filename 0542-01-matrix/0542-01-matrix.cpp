4c63a1988ffed68285bd343c86eaac5981c8bdea
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-04-18 11:37:06 +0530

    Time: 28 ms (24.21%), Space: 38.9 MB (20.37%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-
-        vector<vector<int>> vis(n, vector<int> (m, 0));
-        vector<vector<int>> dist(n, vector<int> (m, 0));
+        int m = mat.size();
+        int n = mat[0].size();
+        
+        vector<vector<int>> dist(m, vector<int> (n, 0));
+        vector<vector<int>> vis(m, vector<int> (n, 0));
 
         queue<pair<pair<int, int>, int>> q;
 
-        for(int i =0; i < n; i++){
-            for(int j =0; j < m; j++){
+        for(int i =0; i < m; i++){
+            for(int j =0; j < n; j++){
                 if(mat[i][j] == 0){
                     q.push({{i, j}, 0});
                     vis[i][j] = 1;
-                }else{
-                    vis[i][j] =0;
                 }
             }
         }
 
-        int delrow[] = {-1, 0, +1, 0};
-        int delcol[] = {0, 1, 0, -1};
-
         while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            
-            int steps = q.front().second;
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+
+            int dis = q.front().second;
+
+            dist[r][c] = dis;
+
             q.pop();
 
-            dist[row][col] = steps;
+            int delrow[] = {-1, 0, 1, 0};
+            int delcol[] = {0, 1, 0, -1};
 
-            for(int i=0; i < 4; i++){
-                int nrow = row+delrow[i];
-                int ncol = col + delcol[i];
+            for(int i =0; i <4; i++){
 
-                if(nrow >= 0 && nrow< n && ncol >=0 && ncol < m && vis[nrow][ncol] == 0){
+                int nrow = r + delrow[i];
+                int ncol = c + delcol[i];
+
+                if(nrow >=0 && nrow < m && ncol >=0 && ncol < n
+                && vis[nrow][ncol] == 0){
+                    q.push({{nrow, ncol}, dis+1});
                     vis[nrow][ncol] =1;
-                    q.push({{nrow,ncol}, steps+1});
                 }
             }
         }
