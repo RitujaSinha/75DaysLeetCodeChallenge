5d82f367fe1234907f1d5574b3296bf2590f56aa
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-04-18 18:40:21 +0530

    Time: 0 ms (100%), Space: 17.2 MB (29.16%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -4,45 +4,49 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
+        vector<vector<int>> vis(m, vector<int> (n, 0));
+
         queue<pair<pair<int, int>, int>> q;
-        
+
         for(int i =0; i < m; i++){
             for(int j =0; j < n; j++){
                 if(grid[i][j] == 2){
                     q.push({{i, j}, 0});
+                    vis[i][j] = 1;
                 }
             }
         }
 
-        int time =0;
-
+        int time = 0;
         while(!q.empty()){
             int r = q.front().first.first;
             int c = q.front().first.second;
-            
+
             int t = q.front().second;
 
-            time= max(time, t);
+            time = max(time, t);
+
             q.pop();
 
-            int delRow[] = {-1, 0, 1, 0};
-            int delCol[] = {0, 1, 0, -1};
+            int delrow[] = {-1, 0, 1, 0};
+            int delcol[] = {0, 1, 0, -1};
 
             for(int i =0; i < 4; i++){
-                int nrow = r + delRow[i];
-                int ncol = c + delCol[i];
 
-                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n &&
-                grid[nrow][ncol] == 1){
-                    grid[nrow][ncol] = 2;
+                int nrow = r + delrow[i];
+                int ncol = c + delcol[i];
+
+                if(nrow >= 0 && nrow < m && ncol >=0 && ncol < n && grid[nrow][ncol] == 1
+                && !vis[nrow][ncol]){
                     q.push({{nrow, ncol}, t+1});
+                    vis[nrow][ncol] = 1;
                 }
             }
         }
 
         for(int i =0; i < m; i++){
             for(int j =0; j < n; j++){
-                if(grid[i][j] == 1){
+                if(grid[i][j] == 1 && !vis[i][j]){
                     return -1;
                 }
             }
