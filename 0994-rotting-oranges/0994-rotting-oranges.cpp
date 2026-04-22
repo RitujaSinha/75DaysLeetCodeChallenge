2b29eda8f4b7c945a356babac250d7111c4f2667
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-04-23 01:59:27 +0530

    Time: 3 ms (27.55%), Space: 17.2 MB (28.88%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -4,49 +4,44 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> vis(m, vector<int> (n, 0));
-
         queue<pair<pair<int, int>, int>> q;
+        vector<vector<int>> vis(m, vector<int> (n, 0));
 
         for(int i =0; i < m; i++){
             for(int j =0; j < n; j++){
                 if(grid[i][j] == 2){
-                    q.push({{i, j}, 0});
                     vis[i][j] = 1;
+                    q.push({{i, j}, 0});
                 }
             }
         }
 
         int time = 0;
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[]= {0, 1, 0, -1};
         while(!q.empty()){
             int r = q.front().first.first;
             int c = q.front().first.second;
-
-            int t = q.front().second;
-
-            time = max(time, t);
-
+            int t= q.front().second;
             q.pop();
 
-            int delrow[] = {-1, 0, 1, 0};
-            int delcol[] = {0, 1, 0, -1};
+            time = max(time, t);
 
             for(int i =0; i < 4; i++){
 
                 int nrow = r + delrow[i];
-                int ncol = c + delcol[i];
+                int ncol = c +delcol[i];
 
-                if(nrow >= 0 && nrow < m && ncol >=0 && ncol < n && grid[nrow][ncol] == 1
-                && !vis[nrow][ncol]){
+                if(nrow >=0 && nrow < m && ncol >= 0 && ncol < n && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
+                    vis[nrow][ncol] =1;
                     q.push({{nrow, ncol}, t+1});
-                    vis[nrow][ncol] = 1;
                 }
             }
         }
 
         for(int i =0; i < m; i++){
             for(int j =0; j < n; j++){
-                if(grid[i][j] == 1 && !vis[i][j]){
+                if(grid[i][j] == 1 && vis[i][j] == 0){
                     return -1;
                 }
             }
