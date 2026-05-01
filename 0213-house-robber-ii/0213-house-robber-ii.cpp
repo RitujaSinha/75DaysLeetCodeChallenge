4d9bcb22232b9688aff60d5298e08ac8a7d3985d
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-05-01 22:22:15 +0530

    Time: 0 ms (100%), Space: 10.9 MB (36%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int t[101];
-    int solve(vector<int> &nums, int n, int i){
+    int solve(vector<int>& nums, int i, int n, vector<int> &t){
         if(i >= n) return 0;
 
         if(t[i] != -1) return t[i];
 
-        int take = nums[i] + solve(nums, n, i+2);
-        int skip = solve(nums, n, i+1);
+        int robb = nums[i] + solve(nums, i+2, n, t);
+        int skip = solve(nums, i+1, n, t);
 
-        return t[i] = max(take, skip); 
+
+        return t[i] = max(skip, robb);
     }
     int rob(vector<int>& nums) {
-        int n = nums.size();
+        int n =nums.size();
 
         if(n==1) return nums[0];
-        if(n==0) return 0;
+        if(n == 2) return max(nums[0], nums[1]);
 
-        memset(t, -1, sizeof(t));
-        int take0thHouse =  solve(nums, n-1, 0);
+        vector<int> dp1(n, -1);
+        int zeroth = solve(nums, 0, n-1, dp1);
 
-        memset(t, -1, sizeof(t));
-        int notTake0thHouse = solve(nums, n, 1);
+        vector<int> dp2(n, -1);
+        int first = solve(nums, 1, n, dp2);
 
-        return max(take0thHouse, notTake0thHouse);
+        return max(first, zeroth);
     }
 };
