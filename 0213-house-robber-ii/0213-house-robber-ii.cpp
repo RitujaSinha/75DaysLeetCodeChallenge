5860c03f6405b3e4c1368fdce52a184c6e2aa21a
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-05-01 22:45:39 +0530

    Time: 0 ms (100%), Space: 10.3 MB (61.03%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
+    int robRange(vector<int>& nums, int l, int r){
+
+        int prevPrev = 0;
+        int prev = 0;
+
+        for(int i = l; i <= r; i++){
+            int curr = max(prev, prevPrev + nums[i]);
+
+            prevPrev = prev;
+            prev= curr;
+        }
+        return prev;
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
 
         if(n == 1) return nums[0];
         if(n ==2) return max(nums[0], nums[1]);
 
-        vector<int> t(n+1, 0);
-
-        t[0] = 0;
-        for(int i =1; i <= n-1; i++){
-            int skip = t[i-1];
-            int robb = nums[i-1] + (i-2>= 0 ? t[i-2] : 0);
-
-            t[i] = max(skip, robb);
-        }
-
-        int res1 = t[n-1];
-
-        t.clear();
-        t[0] = 0;
-        t[1] = 0;
-
-        for(int i = 2; i <= n; i++){
-            int skip = t[i-1];
-            int robb = nums[i-1] + t[i-2];
-
-            t[i] = max(skip, robb);
-        }
-
-        int res2 = t[n];
-
-        return max(res1, res2);
+        return max(robRange(nums, 0, n-2), robRange(nums,1, n-1));
     }
 };
