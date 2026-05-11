01edd8d94d52fa12ffbd44e52e519e75b1049141
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-05-11 16:08:31 +0530

    Time: 0 ms (100%), Space: 12.6 MB (71.03%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -3,22 +3,23 @@ public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
 
-        if(n == 1) return 0;
+        if(n==1) return 0;
 
         if(nums[0] > nums[1]) return 0;
         if(nums[n-1] > nums[n-2]) return n-1;
 
-        int st = 1, end= n-2;
-
+        int st = 1, end = n-2;
         while(st <= end){
-            int mid = st+(end-st)/2;
+            int mid = st +(end-st)/2;
 
-            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+                return mid;
+            }
 
-            if(nums[mid] > nums[mid-1]){//Left Sorted
+            if(nums[mid] >= nums[mid-1]){
                 st = mid+1;
-            } else{//Right Sorted
-                end = mid-1;
+            } else{
+                end=mid-1;
             }
         }
         return -1;
