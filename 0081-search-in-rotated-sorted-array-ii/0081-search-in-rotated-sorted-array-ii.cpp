21170185e9ea163c897e4a53039507d86261b4af
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-05-11 16:25:37 +0530

    Time: 0 ms (100%), Space: 18.1 MB (27.78%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -3,35 +3,33 @@ public:
     bool search(vector<int>& nums, int target) {
         int n = nums.size();
 
-        int st = 0, end = n-1;
+        int st =0, end = n-1;
 
         while(st <= end){
-            int mid = st + (end-st)/2;
+            int mid = st +(end-st)/2;
 
-            if(nums[mid] == target){
-                return true;
-            }
+            if(nums[mid] == target) return true;
 
-            if(nums[st] == nums[mid] && nums[mid] == nums[end]){
-                st = st +1;
-                end = end-1;
+             if(nums[st] == nums[mid] && nums[mid] == nums[end]){
+                st++;
+                end--;
                 continue;
             }
-            
-            if(nums[mid] >= nums[st]){
-                if(nums[st] <= target && target <= nums[mid]){
+
+            if(nums[st] <= nums[mid]){
+                if(nums[st] <= target && target < nums[mid]){
                     end = mid-1;
                 } else{
                     st = mid+1;
                 }
             } else{
-                if(nums[mid] <= target && target <= nums[end]){
+                if(nums[mid] < target && target <= nums[end]){
                     st = mid+1;
                 } else{
-                    end = mid-1;
+                    end= mid-1;
                 }
             }
-        } 
+        }
         return false;
     }
 };
