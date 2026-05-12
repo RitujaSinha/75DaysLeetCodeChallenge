94d7549be9286885a5eceaa6797ec3e7fc0a19cd
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-05-12 16:07:19 +0530

    Time: 23 ms (39.7%), Space: 39 MB (19.97%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,44 +4,44 @@ public:
         int m = mat.size();
         int n = mat[0].size();
 
-        vector<vector<int>> vis(m, vector<int> (n, 0));
-        vector<vector<int>> dist(m, vector<int> (n, 0));
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        vector<vector<int>> result(m, vector<int>(n, 0));
 
         queue<pair<pair<int, int>, int>> q;
-
-        for(int i=0; i < m; i++){
-            for(int j =0; j < n; j++){
-                if(mat[i][j] == 0 && !vis[i][j]){
-                    vis[i][j] = 1;
-                    dist[i][j] = 0;
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 0){
                     q.push({{i, j}, 0});
+                    vis[i][j] = 1;
                 }
             }
         }
-
 
         while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int d = q.front().second;
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+
+            int dis = q.front().second;
             q.pop();
 
-            int delrow[] ={-1, 0, 1, 0};
-            int delcol[] ={0, 1, 0, -1};
+            result[row][col] = dis;
 
-            for(int i =0; i < 4; i++){
-                int nrow = r + delrow[i];
-                int ncol = c + delcol[i];
+            int delrow[] = {-1, 0, 1, 0};
+            int delcol[] = {0, 1, 0, -1};
 
+            for(int i = 0; i < 4; i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
 
-                if(nrow >=0 && nrow< m && ncol >=0 && ncol< n && !vis[nrow][ncol] 
-                && mat[nrow][ncol] == 1){
-                    q.push({{nrow, ncol}, d+1});
-                    dist[nrow][ncol] = d+1;
+                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && mat[nrow][ncol] == 1
+                && !vis[nrow][ncol]){
+                    q.push({{nrow, ncol}, dis+1});
                     vis[nrow][ncol] = 1;
+                    result[nrow][ncol] = dis+1;
                 }
             }
         }
-        return dist;
+        return result;
     }
 };
