cd479144a175da7a6600b35ae002dcbdae42cc3f
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-05-12 09:36:57 +0530

    Time: 16 ms (91.25%), Space: 70.6 MB (11.06%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -2,34 +2,39 @@ class Solution {
 public:
     bool isValid(vector<int>& bloomDay, int m, int k, int days){
         int n = bloomDay.size();
+
         int flowers = 0;
         int bouquet = 0;
 
         for(int i = 0; i < n; i++){
-            if(days >= bloomDay[i]){
+            if(bloomDay[i] <= days){
                 flowers++;
+
+                if(flowers==k){
+                    bouquet++;
+                    flowers = 0;
+                }
             } else{
-                bouquet += (flowers/k);
-                flowers=0;
+                flowers = 0;
             }
         }
-        bouquet += (flowers/k);
-
-        return bouquet>=m;
+        return bouquet >= m;
     }
-
     int minDays(vector<int>& bloomDay, int m, int k) {
-        
+        int n = bloomDay.size();
+
+        if(n < (long long)m*k) return -1;
+
         int st = *min_element(bloomDay.begin(), bloomDay.end());
         int end = *max_element(bloomDay.begin(), bloomDay.end());
 
-        int ans = -1;
+        int ans  = 0;
         while(st <= end){
-            int mid = st+(end-st)/2;
+            int mid = st + (end-st)/2;
 
             if(isValid(bloomDay, m, k, mid)){
                 ans = mid;
-                end = mid-1;
+                end=mid-1;
             } else{
                 st =mid+1;
             }
