d72daa406f2e3f448cf8820d53e97fa30f1ef461
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-05-13 17:39:37 +0530

    Time: 0 ms (100%), Space: 18.6 MB (8.48%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size();
-        int m = image[0].size();
-        vector<vector<int>> vis(n, vector<int> (m, 0));
+        int m = image.size();
+        int n = image[0].size();
+
+        vector<vector<int>> vis(m, vector<int> (n, 0));
+        vector<vector<int>> res = image;
 
         queue<pair<int, int>> q;
         q.push({sr, sc});
-        vis[sr][sc]=1;
+        vis[sr][sc] = 1;
 
         int initialColor = image[sr][sc];
+        res[sr][sc] = color;
 
         while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-
+            int r = q.front().first;
+            int c = q.front().second;
             q.pop();
 
-            int delRow[] ={-1, 0, 1, 0};
-            int delCol[] ={0, 1, 0, -1};
+            int delrow[] = {-1, 0, 1, 0};
+            int delcol[] = {0, 1, 0, -1};
 
-            for(int i =0; i < 4; i++){
-                int nrow = row + delRow[i];
-                int ncol = col + delCol[i];
+            for(int i = 0; i < 4; i++){
+                int nrow = r + delrow[i];
+                int ncol = c + delcol[i];
 
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == initialColor && vis[nrow][ncol] == 0){
-                    vis[nrow][ncol] =1;
+
+                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && 
+                image[nrow][ncol] == initialColor && !vis[nrow][ncol]){
+                    res[nrow][ncol] = color;
+                    vis[nrow][ncol] = 1;
                     q.push({nrow, ncol});
                 }
             }
-            image[row][col] = color;
         }
-        return image;
+
+
+        return res;
     }
 };
