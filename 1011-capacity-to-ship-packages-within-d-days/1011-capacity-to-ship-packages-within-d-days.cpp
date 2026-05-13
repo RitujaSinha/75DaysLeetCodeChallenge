09081354f51d8ed38e9cc846ac147f0e23ed52e9
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-05-13 22:25:50 +0530

    Time: 19 ms (10.31%), Space: 35.1 MB (52.22%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-    bool isValid(vector<int>& weights, int days, int minWeightAllow){
+    bool isValid(vector<int>& weights, int days, int minWeightAllowed){
         int n = weights.size();
 
-        int day=1;
-        int packages  =0;
+        int packages = 0;
+        int day =1;
 
-        for(int i =0; i < n; i++){
-            if(packages+weights[i] <= minWeightAllow){
-                packages +=weights[i];
+        for(int i = 0; i < n; i++){
+            if(weights[i] > minWeightAllowed) return false;
+
+            if(packages + weights[i] <= minWeightAllowed){
+                packages+=weights[i];
             } else{
                 day++;
-                packages= weights[i];
+                packages = weights[i];
             }
         }
         return day <= days;
     }
-
     int shipWithinDays(vector<int>& weights, int days) {
         int n = weights.size();
 
         int st = *max_element(weights.begin(), weights.end());
         int end = accumulate(weights.begin(), weights.end(), 0);
 
-        int ans =0;
+        int ans  = 0;
         while(st <= end){
-            int mid = st +(end-st)/2;
+            int mid = st + (end-st)/2;
 
             if(isValid(weights, days, mid)){
-                ans= mid;
-                end = mid-1;
+                ans = mid;
+                end = mid - 1;
             } else{
-                st=mid+1;
+                st = mid+1;
             }
         }
         return ans;
