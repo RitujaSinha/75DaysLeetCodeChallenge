9d9de67b83d064746c2bda6e563ab95c1eed74ff
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-05-14 09:48:52 +0530

    Time: 0 ms (100%), Space: 8.3 MB (83.42%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int n1 = word1.size();
+        int n2 = word2.size();
+
+        int i = 0, j = 0;
+        string res="";
+        while(i < n1 && j < n2){
+            res+=word1[i];
+            res+=word2[i];
+            i++; j++;
+        }
+
+        while(i < n1){
+            res+=word1[i];
+            i++;
+        }
+
+        while(j < n2){
+            res+=word2[j];
+            j++;
+        }
+        return res;
+    }
+};
