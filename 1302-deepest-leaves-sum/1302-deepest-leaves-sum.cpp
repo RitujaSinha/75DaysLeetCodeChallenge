36644c6250271796327f8f959aaff39cb93eca6b
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-05-14 18:47:44 +0530

    Time: 0 ms (100%), Space: 62.5 MB (46.64%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -14,45 +14,25 @@ public:
     int deepestLeavesSum(TreeNode* root) {
         if(root == NULL) return 0;
 
-        queue<pair<TreeNode*, int>> q;
-        q.push({root, 1});
-        int maxDepth = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int sum = 0;
 
         while(!q.empty()){
             int size = q.size();
+            sum = 0;
 
             for(int i = 0; i < size; i++){
-                TreeNode* node = q.front().first;
-                int depth = q.front().second;
+                TreeNode* node = q.front();
                 q.pop();
 
-                maxDepth = max(depth, maxDepth);
+                sum += node->val;
 
-                if(node->left != NULL) q.push({node->left, depth+1});
-                if(node->right != NULL) q.push({node->right, depth+1});
+                if(node->left != NULL) q.push(node->left);
+                if(node->right != NULL) q.push(node->right);
             }
         }
 
-        queue<pair<TreeNode*, int>> q1;
-        q1.push({root, 1});
-
-        int sum = 0;
-        while(!q1.empty()){
-            int size= q1.size();
-
-            for(int i =0; i < size; i++){
-                TreeNode* node = q1.front().first;
-                int d = q1.front().second;
-                q1.pop();
-
-                if(d == maxDepth){
-                    sum += (node->val);
-                }
-                
-                if(node->left != NULL) q1.push({node->left, d+1});
-                if(node->right != NULL) q1.push({node->right, d+1});
-            }
-        }
         return sum;
     }
 };
