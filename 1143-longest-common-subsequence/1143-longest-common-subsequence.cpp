f4e2ddf3e28b4e1270e941a9a639bd606210738e
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-05-19 18:50:31 +0530

    Time: 48 ms (8.02%), Space: 27.5 MB (26.72%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,40 +1,23 @@
-// class Solution {
-// public:
-        //  Left to right travel
-//     // int solve(string &s1, string &s2, int i, int j){
-//     //     if(i >= s1.size() || j >= s2.length()) return 0;
-
-//     //     if(s1[i] == s2[j]){
-//     //         return 1 + solve(s1, s2, i+1, j+1);
-//     //     }
-
-//     //     return max(solve(s1, s2, i+1, j), solve(s1, s2, i, j+1));
-//     // }
-//     int longestCommonSubsequence(string s1, string s2) {
-//         //   return solve(s1, s2, 0, 0);
-//     }
-// };
-
-//Right to left traversal
 class Solution {
 public:
-    int t[1001][1001];
-    int solve(string &s1, string &s2, int m, int n){
-        if(m==0 || n==0) return 0;
-
-        if(t[m][n] != -1) return t[m][n];
-
-        if(s1[m-1] == s2[n-1]){
-            return t[m][n]= 1 + solve(s1, s2, m-1, n-1);
+    int solve(string &s1, string &s2, int i, int j, 
+        vector<vector<int>> &t){
+        if(i >= s1.size() || j >= s2.size()){
+            return 0;
         }
 
-        return t[m][n] = max(solve(s1, s2, m-1, n), solve(s1, s2, m, n-1));
+        if(t[i][j] != -1) return t[i][j];
+
+        if(s1[i] == s2[j]){
+            return t[i][j] = 1 + solve(s1, s2, i+1, j+1,t);
+        } 
+        return t[i][j] = max(solve(s1, s2, i+1, j, t), solve(s1, s2, i, j+1,t));
     }
     int longestCommonSubsequence(string s1, string s2) {
-        int m = s1.size();
-        int n = s2.size();
-        memset(t, -1, sizeof(t));
-        
-        return solve(s1, s2, m, n);
+        int n1= s1.size();
+        int n2 = s2.size();
+
+        vector<vector<int>> t(n1,vector<int> (n2, -1));
+        return solve(s1, s2, 0, 0, t);
     }
 };
