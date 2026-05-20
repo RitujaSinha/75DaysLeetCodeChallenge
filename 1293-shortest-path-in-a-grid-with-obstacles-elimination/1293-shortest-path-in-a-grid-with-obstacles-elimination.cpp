309b6009184d0162425222861fa3455d7f5c550d
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-05-20 23:28:56 +0530

    Time: 42 ms (50.68%), Space: 38.3 MB (34.63%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -4,42 +4,36 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        priority_queue<pair<int, pair<int,pair<int, int>>>, 
-        vector<pair<int, pair<int,pair<int, int>>>>,
-        greater<pair<int, pair<int,pair<int, int>>>>> pq;
+        vector<vector<vector<int>>> dist(m, vector<vector<int>>(n, vector<int> (k+1, INT_MAX)));
 
-        pq.push({0,{0, {0, 0}}});//{steps,{k{row, col}}}
-
-        vector<vector<vector<int>>> dist(m, vector<vector<int>>(n, vector<int>(k+1, INT_MAX)));
+        queue<pair<int, pair<int, pair<int, int>>>> q;
+        q.push({0, {0, {0, 0}}});
         dist[0][0][0] = 0;
-
 
         int delrow[] = {-1, 0, 1, 0};
         int delcol[] = {0, 1, 0, -1};
-
-        while(!pq.empty()){
-            int steps = pq.top().first;
-            int obstacles = pq.top().second.first;
-            int row = pq.top().second.second.first;
-            int col = pq.top().second.second.second;
-            pq.pop();
-            if(steps > dist[row][col][obstacles]) continue;
-
-            if(obstacles > k) continue;
+        while(!q.empty()){
+            int steps = q.front().first;
+            int obstacles = q.front().second.first;
+            int row = q.front().second.second.first;
+            int col = q.front().second.second.second;
+            q.pop();
 
             if(row == m-1 && col == n-1) return steps;
+
+            if(obstacles > k) continue;
 
             for(int i = 0; i < 4; i++){
                 int nrow = row + delrow[i];
                 int ncol = col + delcol[i];
 
                 if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n){
-                    
+
                     int newObstacle = obstacles + grid[nrow][ncol];
-                    
-                    if(newObstacle <= k && dist[nrow][ncol][obstacles] > steps+1){
-                        dist[nrow][ncol][obstacles] = steps+1;
-                        pq.push({steps+1,{newObstacle,{nrow, ncol}}});
+
+                    if(newObstacle <= k  && steps+1 < dist[nrow][ncol][newObstacle]){
+                        dist[nrow][ncol][newObstacle]= steps+1;
+                        q.push({steps+1, {newObstacle,{ nrow, ncol}}});
                     }
                 }
             }
