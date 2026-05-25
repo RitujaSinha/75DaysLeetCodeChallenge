8461fc0c64a1802b994d21b16bc22ef21671d0c3
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-05-25 22:37:00 +0530

    Time: 8 ms (79.34%), Space: 225.8 MB (20.07%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.cpp
+++ 3660-jump-game-ix/3660-jump-game-ix.cpp
@@ -4,23 +4,24 @@ public:
         int n = nums.size();
 
         vector<int> pref(n), suff(n);
-
+        
         pref[0] = nums[0];
         for(int i = 1; i < n; i++){
             pref[i] = max(pref[i-1], nums[i]);
         }
 
         suff[n-1] = nums[n-1];
-        for(int i = n-2; i>= 0; i--){
+        for(int i =n-2; i>=0; i--){
             suff[i] = min(suff[i+1], nums[i]);
         }
 
         vector<int> ans(n);
-        ans[n-1] = pref[n-1]; //wo last element hai.. usse chota koi element nahi hai aage jisse jump kr sake
-        for(int i = n-2; i>=0; i--){
-            ans[i] = pref[i];
+        ans[n-1] = pref[n-1];
 
-            if(pref[i] > suff[i+1]){
+        for(int i = n-2; i>=0; i--){
+            if(pref[i] <= suff[i+1]){
+                ans[i] = pref[i];
+            } else{
                 ans[i] = ans[i+1];
             }
         }
