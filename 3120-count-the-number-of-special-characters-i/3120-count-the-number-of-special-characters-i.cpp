ccf8b7de7cacef8e1af3064d2ee813164ed85ec4
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-05-26 09:59:12 +0530

    Time: 0 ms (100%), Space: 9 MB (68.68%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -2,28 +2,15 @@ class Solution {
 public:
     int numberOfSpecialChars(string word) {
         
-        // vector<int> freq1(26), freq2(26);
-        // for(char c: word){
-        //     if(c>='a' && c<='z') freq1[c-'a'] = 1;
-        //     else freq2[c-'a'] = 1;
-        // }
-
-        int count =0;
-        // for(int i = 0; i < 26; i++){
-        //     if(freq1[i] == 1 && freq2[i] == 1){
-        //         count++;
-        //     }
-        // }
-        
-        unordered_map<char, int> m;
-        set<char> st;
-        for(char ch: word){
-            m[ch]++;
-            if(ch>='a' && ch<='z') st.insert(ch);
+        vector<bool> lower(26, false), upper(26, false);
+        for(char c: word){
+            if(islower(c)) lower[c-'a'] = 1;
+            else upper[c-'A'] = 1;
         }
 
-        for(auto it: st){
-            if(m.find(it-32) != m.end()){
+        int count =0;
+        for(int i = 0; i < 26; i++){
+            if(lower[i] == 1 &&  upper[i] == 1){
                 count++;
             }
         }
