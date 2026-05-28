648b54467169634f87ee1946d0bb4ea3a4ef6e8b
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-05-28 12:12:22 +0530

    Time: 195 ms (72.07%), Space: 10.4 MB (92.65%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,44 +1,48 @@
 class Solution {
 public:
-    bool find(vector<vector<char>>& board,int i, int j, int idx, string& word){
+    bool find(vector<vector<char>>& board,int i, int j,int idx, string &word){
         int m = board.size();
         int n = board[0].size();
 
-        if(i < 0 || j < 0 || i>= m || j >= n || board[i][j] == '$'){
-            return false;
+        if(idx== word.size()-1){
+            return true;
         }
 
-        if(board[i][j] != word[idx]) return false;
-
-         if(idx == word.size()-1){
-            return true;
+        if(word[idx] != board[i][j]){
+            return false;
         }
 
         char temp = board[i][j];
         board[i][j] = '$';
-
+        
         int delrow[] = {-1, 0, 1, 0};
         int delcol[] = {0, 1, 0, -1};
-        for(int dir =0; dir < 4; dir++){
+
+        for(int dir = 0; dir < 4; dir++){
             int nrow = i + delrow[dir];
             int ncol = j + delcol[dir];
 
-            if(nrow >=0 && nrow < m && ncol >=0 && ncol < n){
-                if(find(board, nrow, ncol, idx+1, word)) return true;
+            if(nrow >=0 && ncol >=0 && nrow < m && ncol < n){
+                if(board[nrow][ncol] == word[idx+1]){
+                    if(find(board, nrow, ncol, idx+1, word)) return true;
+                }
             }
-        }
-        board[i][j] = temp;
+        } 
 
+        board[i][j] = temp; 
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size();
         int n = board[0].size();
 
-        for(int i = 0; i <m; i++){
-            for(int j = 0; j < n; j++){
-                if(board[i][j] == word[0] && find(board, i, j, 0, word)){ //pehle word ke char match hua to check krege 4 side
-                    return true;
+
+        for(int i =0; i < m; i++){
+            for(int j =0; j < n; j++){
+                if(board[i][j] == word[0]){
+                    if(find(board, i, j,0, word)){
+                        return true;
+                    }
                 }
             }
         }
