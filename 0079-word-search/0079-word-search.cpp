74ad7f5f4cac5c11a416a2f1256ccb568d654f67
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-01 10:47:02 +0530

    Time: 347 ms (44.02%), Space: 11.7 MB (29.74%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    bool find(vector<vector<char>>& board,int i, int j,int idx, string &word){
-        int m = board.size();
-        int n = board[0].size();
+    bool find(vector<vector<char>>& grid, int i, int j, int idx, string word){
+        int m = grid.size();
+        int n = grid[0].size();
 
-        if(idx== word.size()-1){
+        if(idx == word.size()-1){
             return true;
         }
 
-        if(word[idx] != board[i][j]){
-            return false;
-        }
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '$') return false;
 
-        char temp = board[i][j];
-        board[i][j] = '$';
-        
+        if(grid[i][j] != word[idx]) return false;
+
+        char temp = grid[i][j];
+        grid[i][j] = '$';
+
         int delrow[] = {-1, 0, 1, 0};
         int delcol[] = {0, 1, 0, -1};
 
@@ -22,27 +22,25 @@ public:
             int nrow = i + delrow[dir];
             int ncol = j + delcol[dir];
 
-            if(nrow >=0 && ncol >=0 && nrow < m && ncol < n){
-                if(board[nrow][ncol] == word[idx+1]){
-                    if(find(board, nrow, ncol, idx+1, word)) return true;
+            if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n){
+                if(grid[nrow][ncol] == word[idx+1]){
+                    if(find(grid, nrow, ncol, idx+1, word)) return true;
                 }
             }
         } 
 
-        board[i][j] = temp; 
+        grid[i][j] = temp;
+
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size();
         int n = board[0].size();
 
-
         for(int i =0; i < m; i++){
             for(int j =0; j < n; j++){
-                if(board[i][j] == word[0]){
-                    if(find(board, i, j,0, word)){
-                        return true;
-                    }
+                if(board[i][j] == word[0] && find(board, i, j,0, word)){
+                    return true;
                 }
             }
         }
