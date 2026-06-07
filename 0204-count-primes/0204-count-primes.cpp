350ebc30ca17b86d1561d323a104575ca9f91b35
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-07 18:47:36 +0530

    Time: 394 ms (7.39%), Space: 12.8 MB (35.74%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        
+        vector<bool> isPrime(n+1, true);
+
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for(int i = 2; i * i < n; i++){
+            for(int j = i*i; j <= n; j+=i){
+                isPrime[j] = false;
+            }
+        }
+
+        int count= 0;
+        for(int i = 0; i < isPrime.size()-1; i++){
+            if(isPrime[i]) count++;
+        }
+        return count;
+    }
+};
