66de9134792a25d95a2b55cf26c9a5ef3e3e41b0
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-08 17:21:19 +0530

    Time: 0 ms (100%), Space: 7.9 MB (70.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,20 +4,25 @@ public:
 
         if(n==1 || n== 2) return n;
         
-        vector<int> dp(n+1, 0); // steps to reach n stairs
-        dp[1] = 1; // 1 steps to reach 1 stair
-        dp[2] = 2;
+        int prevPrev = 1; // 1 steps to reach 1 stair
+        int prev = 2;
 
         // i+1, i+2
 
         // -1, 0 // out of bound
 
         for(int i = 3; i < n+1; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prevPrev + prev;
+
+
+            prevPrev = prev;
+            prev = curr;
         }
-        return dp[n];
+        return prev;
     }
 };
 
+// 3
 
+// 1, 2, 3
 
