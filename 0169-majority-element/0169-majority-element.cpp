f9547cdff47e5ca3bab71281976c486fdfe4255e
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-08 14:08:25 +0530

    Time: 1995 ms (5.03%), Space: 28.3 MB (26.87%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        
-        int n =nums.size();
+        int n = nums.size();
 
-        for(int i =0; i < n; i++){
-            int freq =0;
-            for(int j =i; j < n; j++){
-                if(nums[i] ==nums[j]) freq++;
+        for(int i = 0; i < n; i++){
+            int count = 0;
+            for(int j = 0; j < n; j++){
+                if(nums[i] == nums[j]) count++;
 
-                if(freq > n/2) return nums[j];
+                if(count > n/2) return nums[i];
             }
         }
         return -1;
     }
 };
+
+
+// n/2 times
+
+// i
+// 3, 3, 2, 4, 3
+             
+// count = 3;
+
+// 3 >5/2
+// 3 is majprity element
