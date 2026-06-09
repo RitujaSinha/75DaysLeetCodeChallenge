01f3c22d64c26071ffef648c3233ac25785582fe
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-09 19:52:29 +0530

    Time: 119 ms (5.14%), Space: 81.3 MB (5.11%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -3,15 +3,22 @@ public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int V = graph.size();
 
-        vector<vector<int>> adjRev(V);
+        vector<vector<int>> adj(V);
         for(int i = 0; i < V; i++){
             for(auto it: graph[i]){
+                adj[i].push_back(it);
+            }
+        }
+
+        vector<vector<int>> adjRev(V);
+        for(int i = 0; i < V; i++){
+            for(auto it: adj[i]){
                 adjRev[it].push_back(i);
             }
         }
 
-        vector<int> indegree(V, 0);
-        for(int i = 0; i< V; i++){
+        vector<int> indegree(V);
+        for(int i=0; i < V; i++){
             for(auto it: adjRev[i]){
                 indegree[it]++;
             }
@@ -25,23 +32,21 @@ public:
             }
         }
 
-        vector<int> safeNodes;
+        vector<int> topo;
 
         while(!q.empty()){
-            int node = q.front();
+            int node= q.front();
             q.pop();
 
-            safeNodes.push_back(node);
+            topo.push_back(node);
 
             for(auto it: adjRev[node]){
                 indegree[it]--;
 
-                if(indegree[it] == 0){
-                    q.push(it);
-                }
+                if(indegree[it]==0) q.push(it);
             }
         }
-        sort(safeNodes.begin(), safeNodes.end());
-        return safeNodes;
+        sort(topo.begin(), topo.end());
+        return topo;
     }
 };
