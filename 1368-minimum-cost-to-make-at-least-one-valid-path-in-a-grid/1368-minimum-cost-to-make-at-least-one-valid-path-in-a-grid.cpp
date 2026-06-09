2d177a644fd72a96c0357cbdd5650b9632f5a49b
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-09 20:24:12 +0530

    Time: 15 ms (71.12%), Space: 18.2 MB (93.83%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -4,56 +4,50 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> dist(m, vector<int> (n, INT_MAX));
-
         priority_queue<pair<int, pair<int, int>>,
         vector<pair<int, pair<int, int>>>,
         greater<pair<int, pair<int, int>>>> pq;
 
-        pq.push({0, {0, 0}});
-        dist[0][0] = 0;
+        pq.push({0, {0, 0}}); //cost, row, col
+
+        vector<vector<int>> cost(m, vector<int> (n, INT_MAX));
+
+        cost[0][0] = 0; 
 
         while(!pq.empty()){
             int currCost = pq.top().first;
             int row = pq.top().second.first;
             int col = pq.top().second.second;
+
             pq.pop();
 
-            int delrow[] = {-1, 0, 1, 0};
-            int delcol[] = {0, 1, 0, -1};
+            if(row == m-1 && col == n-1) return currCost;
+
+            if(cost[row][col] < currCost) continue;
+
+            int delrow[] = {0,0, 1, -1};
+            int delcol[] = {1, -1, 0, 0};
 
             for(int i =0; i < 4; i++){
-                int nrow = row + delrow[i];
+                int nrow =  row + delrow[i];
                 int ncol = col + delcol[i];
 
-                if(nrow >=0 && nrow < m && ncol >=0 && ncol < n){
+                if(nrow >=0 && nrow < m && ncol>=0 && ncol < n){
 
-                    int extraCost = 1;
-                    
-                    if(grid[row][col] == 1 && row == nrow && ncol == col+1){
-                        extraCost =0; //right
+                    int extraCost = 0;
+                    if(grid[row][col]-1 != i){
+                        extraCost +=1;
                     }
 
-                    if(grid[row][col] == 2 && row == nrow && ncol == col-1){
-                        extraCost =0; //left
-                    }
+                    int newCost = currCost + extraCost;
 
-                    if(grid[row][col] == 3 && row+1 == nrow && ncol == col){
-                        extraCost =0; //lower
-                    }
-                    if(grid[row][col] == 4 && row-1 == nrow && ncol == col){
-                        extraCost =0; //upper
-                    }
-
-                    int newCost = extraCost + currCost;
-
-                    if(newCost < dist[nrow][ncol]){
-                        dist[nrow][ncol] = newCost;
+                    if(cost[nrow][ncol] > newCost){
+                        cost[nrow][ncol] = newCost;
                         pq.push({newCost, {nrow, ncol}});
                     }
                 }
             }
         }
-        return dist[m-1][n-1];
+        return -1;
     }
 };
