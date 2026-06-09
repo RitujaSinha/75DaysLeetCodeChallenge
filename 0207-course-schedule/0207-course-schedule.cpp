53fd33e7b2aef5e70624e52e35419cd38bbcc243
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-09 11:53:14 +0530

    Time: 11 ms (21.31%), Space: 20.1 MB (32.75%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -3,12 +3,11 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         
         vector<vector<int>> adj(numCourses);
-        for(auto it: prerequisites){//created adj list
-            adj[it[1]].push_back(it[0]);
+        for(auto it: prerequisites){
+            adj[it[0]].push_back(it[1]);
         }
 
-        vector<int> indegree(numCourses, 0);
-
+        vector<int> indegree(numCourses);
         for(int i = 0; i < numCourses; i++){
             for(auto it: adj[i]){
                 indegree[it]++;
@@ -17,28 +16,25 @@ public:
 
         queue<int> q;
 
-        for(int i = 0; i < numCourses; i++){
-            if(indegree[i] == 0){
+        for(int i=0; i < numCourses; i++){
+            if(indegree[i]==0){
                 q.push(i);
             }
         }
 
-        vector<int> topo;
-
+        int cnt = 0;
         while(!q.empty()){
-            int node = q.front();
+            int node= q.front();
             q.pop();
 
-            topo.push_back(node);
+            cnt++;
 
             for(auto it: adj[node]){
                 indegree[it]--;
 
-                if(indegree[it] == 0){
-                    q.push(it);
-                }
+                if(indegree[it]==0) q.push(it);
             }
         }
-        return topo.size() == numCourses;
+        return cnt==numCourses;
     }
 };
