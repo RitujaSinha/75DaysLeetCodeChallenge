e3f7b988ec6354c1f06ede0c4638c315fc791927
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-10 23:44:05 +0530

    Time: 0 ms (100%), Space: 12.6 MB (8.28%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int n1 = nums1.size();
-
-        int i = m-1;
-        int j = n-1;
-        int idx = m+n-1;
-
-        while(i >=0 && j >=0){
-            if(nums1[i] > nums2[j]){
-                nums1[idx--] = nums1[i--];
-            } else if(nums1[i] < nums2[j]){
-                nums1[idx--] = nums2[j--];
+        
+        vector<int> sort(m+n);
+        int i = 0, j =0;
+        int idx =0;
+        while(i < m && j < n){
+            if(nums1[i] <= nums2[j]){
+                sort[idx++] = nums1[i++];
             } else{
-                nums1[idx--] = nums2[j--];
+                sort[idx++] = nums2[j++];
             }
         }
 
-        while(j >=0){
-            nums1[idx--] = nums2[j--]; 
+        while(j < n){
+            sort[idx++] = nums2[j++];
         }
-        // return nums1;
+
+        while(i < m){
+            sort[idx++] = nums1[i++];
+        }
+
+        
+        for(int i = 0; i < m+n; i++){
+            nums1[i] = sort[i];
+        }
     }
 };
