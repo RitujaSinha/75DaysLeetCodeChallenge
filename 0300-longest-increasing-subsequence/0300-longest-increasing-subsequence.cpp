670464a7a93a0e3a50388c10ab3184197453f275
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-11 05:35:13 +0530

    Time: 559 ms (14.35%), Space: 296.4 MB (18.6%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
+    int solve(int i, int P, vector<int>& nums, vector<vector<int>> &t){
+        if(i == nums.size()){
+            return 0;
+        }
+
+        if(t[i][P+1] != -1) return t[i][P+1];
+
+        int take =0;
+        if(P == -1 || nums[P] < nums[i]){
+            take = 1 + solve(i+1, i, nums, t);
+        }
+        int notTake = solve(i+1, P, nums, t);
+
+        return t[i][P+1] = max(take, notTake);
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> sorted;
-
-        for(int i = 0; i < n; i++){
-
-            auto it = lower_bound(sorted.begin(), sorted.end(), nums[i]);
-
-            if(it == sorted.end()){
-                sorted.push_back(nums[i]);
-            } else{
-                *it = nums[i];
-            }
-        }
-        return sorted.size();
+        vector<vector<int>> t(n, vector<int> (n+1, -1));
+        return solve(0, -1, nums, t);
     }
 };
