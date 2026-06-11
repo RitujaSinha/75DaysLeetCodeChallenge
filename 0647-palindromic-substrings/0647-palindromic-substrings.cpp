1f8d9842245409b863ccca4a2428b3911c3cfe89
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-11 05:51:49 +0530

    Time: 375 ms (16.71%), Space: 8.7 MB (59.85%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
+    bool isPalin(int i, int j, string &s){
+        if(i >= j){
+            return true;
+        }
+
+        if(s[i] != s[j]){
+            return false;
+        } 
+        return isPalin(i+1, j-1, s);
+    }
     int countSubstrings(string s) {
         int n = s.size();
 
-        vector<vector<bool>> t(n, vector<bool> (n, false));
-
-        //fill 1 len
         int count = 0;
+
         for(int i = 0; i < n; i++){
-            t[i][i] = true;
-            count++;
-        }
-
-        for(int L=2; L<= n; L++){
-            for(int i = 0; i+L-1< n; i++){
-                int j = i+L-1;
-
-                if(L==2 && i+1==j){
-                    t[i][j] = (s[i]==s[j]);
-                } else{
-                    t[i][j] = (s[i]==s[j] && t[i+1][j-1]);
-                }
-
-                if(t[i][j]){
-                    count++;
-                }
-            }   
+            for(int j  =i; j < n; j++){
+                if(isPalin(i, j, s)) count++;
+            }
         }
         return count;
     }
