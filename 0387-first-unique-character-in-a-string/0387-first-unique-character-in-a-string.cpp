6fd0e5e3e313596f1f8d4cd923d8413f70a18e64
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-13 08:40:21 +0530

    Time: 0 ms (100%), Space: 14.8 MB (54.1%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -3,15 +3,13 @@ public:
     int firstUniqChar(string s) {
         int n = s.size();
 
-        vector<char> freq(26, 0);
-        for(int i  =0; i < n; i++){
-            freq[s[i]-'a']++;
+        vector<int> freq(26, 0);
+        for(char &c: s){
+            freq[c-'a']++;
         }
 
-        for(int i =0; i < n; i++){
-            if(freq[s[i]-'a']==1){
-                return i;
-            }
+        for(int i=0; i < n; i++){
+            if(freq[s[i]-'a'] == 1) return i;
         }
         return -1;
     }
