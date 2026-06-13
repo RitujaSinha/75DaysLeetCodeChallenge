3e2f3fe10b6495011e8a9070e5f509bc1f976791
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-13 21:13:43 +0530

    Time: 155 ms (33.61%), Space: 106 MB (29.01%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -4,38 +4,46 @@ public:
         return a.size() < b.size();
     }
 
-    bool isPred(string &curr, string &prev){
-        int m = curr.size();
-        int n = prev.size();
+    bool isPred(string &prev, string &curr){
+        int m = prev.size();
+        int n = curr.size();
 
-        if(m != n+1) return false;
+        if(n != m+1) return false;
 
         int i = m-1, j=n-1;
-
-        while(j >= 0 && i >=0){
-            if(curr[i] == prev[j]){
+        while(i>=0 && j>=0){
+            if(prev[i] == curr[j]){
                 i--; j--;
             } else{
-                i--;
+                j--;
             }
         }
-        return j < 0;
+        return i < 0;
+    }
+
+    int solve(int i,int P, vector<string>& words, vector<vector<int>> &t){
+        if(i == words.size()){
+            return 0;
+        }
+
+        if(t[i][P+1] !=-1) return t[i][P+1]; 
+
+
+        int take = 0;
+        if(P== -1 || isPred(words[P], words[i])){
+            take = 1 + solve(i+1, i, words, t);
+        }
+        int notTake = solve(i+1, P, words, t);
+
+        return t[i][P+1] = max(take, notTake);
     }
     int longestStrChain(vector<string>& words) {
         int n = words.size();
-        if(n==1) return 1;
 
         sort(words.begin(), words.end(), comp);
 
-        vector<int> t(n, 1);
+        vector<vector<int>> t(n, vector<int> (n+1, -1));
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(isPred(words[i], words[j])){
-                    t[i] = max(t[i], t[j]+1);
-                }
-            }
-        }
-        return *max_element(t.begin(), t.end());
+        return solve(0,-1, words, t);
     }
 };
