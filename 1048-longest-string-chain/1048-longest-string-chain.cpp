f2c1bbbf166be46a342da60df3137daee5eb3de9
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-13 21:05:42 +0530

    Time: 40 ms (71.82%), Space: 18.6 MB (95.76%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,42 +1,41 @@
 class Solution {
 public:
-    static bool comp(string &s1, string &s2){
-        return s1.size() < s2.size();
+    static bool comp(string &a, string &b){
+        return a.size() < b.size();
     }
 
-    bool isPred(string &s1, string &s2){
-        int n = s1.size();
-        int m = s2.size();
+    bool isPred(string &curr, string &prev){
+        int m = curr.size();
+        int n = prev.size();
 
-        if(n != m+1) return false;
+        if(m != n+1) return false;
 
-        int i=0, j=0;
-        while(i < n && j < m){
-            if(s1[i] == s2[j]){
-                j++;
+        int i = m-1, j=n-1;
+
+        while(j >= 0 && i >=0){
+            if(curr[i] == prev[j]){
+                i--; j--;
+            } else{
+                i--;
             }
-            i++;
         }
-        return j == m;
+        return j < 0;
     }
-
     int longestStrChain(vector<string>& words) {
         int n = words.size();
+        if(n==1) return 1;
 
         sort(words.begin(), words.end(), comp);
 
         vector<int> t(n, 1);
 
-        int maxL = 1;
-
-        for(int i =0; i < n; i++){
-            for(int j =0; j < i; j++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
                 if(isPred(words[i], words[j])){
                     t[i] = max(t[i], t[j]+1);
-                    maxL = max(maxL, t[i]);
                 }
             }
         }
-        return maxL;
+        return *max_element(t.begin(), t.end());
     }
 };
