47637e8897d7b199fb1efd102fb8ec6ae4886207
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-15 21:01:22 +0530

    Time: 3 ms (16.89%), Space: 27.2 MB (26.75%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,7 +4,7 @@ public:
         int n = height.size();
 
         vector<int> leftMax(n), rightMax(n);
-
+        
         leftMax[0] = height[0];
         for(int i = 1; i < n; i++){
             leftMax[i] = max(leftMax[i-1], height[i]);
@@ -17,7 +17,7 @@ public:
 
         int total = 0;
         for(int i =0; i < n; i++){
-            if(height[i] < leftMax[i] && height[i] < rightMax[i]){
+            if(leftMax[i] > height[i] && rightMax[i] > height[i]){
                 total += min(leftMax[i], rightMax[i])- height[i];
             }
         }
