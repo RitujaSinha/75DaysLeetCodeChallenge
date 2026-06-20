ff0e39de95227e97e9e072a5a579b932ee036a69
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-20 15:10:07 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.24%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -4,19 +4,20 @@ public:
         int n = s.size();
 
         stack<char> st;
-
-        for(int i =0; i < n; i++){
+        for(int i = 0; i < n; i++){
             if(s[i] == '(' || s[i] == '{' || s[i] == '['){
                 st.push(s[i]);
             } else{
                 if(st.empty()) return false;
-
+                
                 char ch = st.top();
                 st.pop();
 
-                if((s[i] == '}' && ch != '{') || 
-                (s[i] == ']' && ch != '[') ||
-                (s[i] == ')' && ch != '(') ) return false;
+                if((ch == '(' && s[i] != ')') ||
+                (ch == '{' && s[i] != '}') ||
+                (ch == '[' && s[i] != ']')){
+                    return false;
+                }
             }
         }
         return st.empty();
