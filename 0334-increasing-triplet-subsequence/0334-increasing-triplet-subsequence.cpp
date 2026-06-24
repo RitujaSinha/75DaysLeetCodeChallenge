3740e5df4cafa899a6dfbebc0dd57526ff2e7357
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-24 22:10:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> t(n, 1);
+
+        int el = nums[0];
+        int cnt = 0;
+        for(int num: nums){
+            if(el == num) cnt++;
+        }
+        if(cnt == n) return false;
+
+        if(nums[n-1] > nums[n-2] && nums[n-2] > nums[n-3]) return true; 
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    t[i] = max(t[i], t[j]+1);
+                }
+
+                if(t[i] == 3) return true;
+            }
+        }
+        return false;
+    }
+};
