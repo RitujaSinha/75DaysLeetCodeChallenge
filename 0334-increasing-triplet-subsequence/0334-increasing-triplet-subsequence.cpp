94c1732f5cce046e59c964b160c5d1186a48e43e
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-24 22:13:32 +0530

    Time: 157 ms (5.57%), Space: 121.3 MB (7.86%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -6,16 +6,12 @@ public:
         vector<int> t(n, 1);
 
         unordered_map<int,int> mpp;
-        // int el = nums[0];
-        // int cnt = 0;
         for(int num: nums){
-            // if(el == num) cnt++;
             mpp[num]++;
         }
-        // if(cnt == n) return false;
-        if(mpp.size() < 3) return false;
+        if(mpp.size() < 3) return false; // if only 2 value is there will be no triplet form
 
-        if(nums[n-1] > nums[n-2] && nums[n-2] > nums[n-3]) return true; 
+        if(nums[n-1] > nums[n-2] && nums[n-2] > nums[n-3]) return true; // manually handled this case
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < i; j++){
