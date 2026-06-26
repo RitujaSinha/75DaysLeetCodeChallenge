69711b9d5f28f756422afa45a356e83f3ae5e784
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-26 19:18:28 +0530

    Time: 0 ms (100%), Space: 15.1 MB (67.61%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    int findMin(vector<int> &nums){
-        int n = nums.size();
-
-        int mini = INT_MAX;
-        int index = -1;
-        for(int i = 0; i < n; i++){
-            if(mini > nums[i]){
-                mini = nums[i];
-                index = i;
-            }
-        }
-        return index;
-    }
     vector<int> luckyNumbers(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
 
-        vector<int> res;
+
+        vector<int> ans;
 
         for(int i = 0; i < m; i++){
-            int index = findMin(matrix[i]);
-            int minEl = matrix[i][index];
-
-            int maxEl = -1;
-            for(int j = 0; j < m; j++){
-                if(matrix[j][index] > maxEl){
-                    maxEl = matrix[j][index];
+            int idx = -1, mini = INT_MAX;
+            for(int j = 0; j < n; j++){
+                if(mini > matrix[i][j]){
+                    mini = matrix[i][j];
+                    idx = j;
                 }
             }
 
-            if(maxEl == minEl) res.push_back(minEl);
+            int maxi = INT_MIN;
+            for(int k = 0; k < m; k++){
+                maxi = max(maxi, matrix[k][idx]);
+            }
+
+            if(mini == maxi){
+                ans.push_back(mini);
+            }
         }
-        return res;
+        return ans;
     }
 };
