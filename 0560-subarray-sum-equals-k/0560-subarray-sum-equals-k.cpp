90bd5c58ce028b421d7bc95ce36db4112b72c698
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-06-30 10:46:28 +0530

    Time: 42 ms (61.11%), Space: 45.3 MB (78.51%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,20 +3,22 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
 
-        vector<int> prefixS(n, 0);
-        prefixS[0] = nums[0];
-        for(int i = 1; i < n; i++){
-            prefixS[i] = prefixS[i-1] + nums[i];
-        }
+        // vector<int> prefixS(n, 0);
+        // prefixS[0] = nums[0];
+        // // for(int i = 1; i < n; i++){
+        // //     prefixS[i] = prefixS[i-1] + nums[i];
+        // // }
 
         unordered_map<int, int> mpp;
 
         int cnt = 0;
+        int currSum =0;
         for(int j = 0; j < n; j++){
+            currSum += nums[j];
 
-            if(prefixS[j] == k) cnt++;
+            if(currSum == k) cnt++;
 
-            int val = prefixS[j] - k;
+            int val = currSum - k;
 
             if(mpp.find(val) != mpp.end()){
                 cnt += mpp[val];
@@ -25,7 +27,7 @@ public:
             //     mpp[prefixS[j]] = 0;
             // }
 
-            mpp[prefixS[j]]++;
+            mpp[currSum]++;
         }
         return cnt;
     }
