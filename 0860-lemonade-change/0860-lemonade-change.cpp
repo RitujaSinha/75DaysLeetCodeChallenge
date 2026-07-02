ddb20f0eb08caabb13e2b17ebb189068240b12ac
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-07-02 23:33:13 +0530

    Time: 0 ms (100%), Space: 87.1 MB (96.77%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -3,27 +3,30 @@ public:
     bool lemonadeChange(vector<int>& bills) {
         int n = bills.size();
 
-        int five =0, ten =0;
-
-        for(int i =0; i < n; i++){
-            if(bills[i] ==5) five++;
+        int five =0, ten = 0, tw = 0;
+        for(int i = 0; i < n; i++){
+            if(bills[i] == 5) five++;
             else if(bills[i] == 10){
-
-                if(five){
+                ten++;
+                if(five > 0){
                     five--;
-                    ten++;
                 } else{
                     return false;
                 }
-            } 
-            else{
-                if(ten > 0 & five > 0){
+            } else{
+                if(ten > 0){
                     ten--;
-                    five--;
-                } else if(five >= 3){
-                    five-=3;
+                    if(five > 0){
+                        five--;
+                    } else{
+                        return false;
+                    }
                 } else{
-                    return false;
+                    if(five >= 3){
+                        five-=3;
+                    } else{
+                        return false;
+                    }
                 }
             }
         }
