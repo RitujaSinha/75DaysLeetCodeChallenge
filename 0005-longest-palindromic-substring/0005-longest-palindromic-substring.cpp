788cf867507cf87cdd3664603de5e0933e18560e
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-07-07 23:34:12 +0530

    Time: 335 ms (14.68%), Space: 26.2 MB (26.87%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -3,34 +3,33 @@ public:
     string longestPalindrome(string s) {
         int n = s.size();
 
-        vector<vector<bool>> t(n, vector<bool> (n, 0));
+        vector<vector<bool>> t(n+1, vector<bool> (n+1, false));
 
+        int maxL = 1;
         int idx = -1;
-        int maxP = 1;
         for(int i = 0; i < n; i++){
             t[i][i] = true;
             idx = i;
         }
 
-
-        for(int L=2; L<= n; L++){
-            for(int i = 0; i+L-1 < n; i++){
+        for(int L = 2; L<=n; L++){
+            for(int i = 0; i + L-1 < n; i++){
                 int j = i+L-1;
 
-                if(i+1==j){
-                    t[i][j] = (s[i]==s[j]);
+                if(i+1 == j){
+                    t[i][j] = (s[i] == s[j]);
                 } else{
                     t[i][j] = (s[i]==s[j] && t[i+1][j-1]);
                 }
 
                 if(t[i][j]){
-                    if(maxP <= j-i+1){
+                    if(maxL < j-i+1){
+                        maxL = j-i+1;
                         idx = i;
-                        maxP = j-i+1;
                     }
                 }
             }
         }
-        return s.substr(idx, maxP);
+        return s.substr(idx, maxL);
     }
 };
