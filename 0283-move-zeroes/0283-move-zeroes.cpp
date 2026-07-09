16b20975cd28752a9414a3b6fce8ba4b89e24d00
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-07-09 22:54:49 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.95%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,13 +3,11 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
 
-        int i = 0, j = i+1;
+        int i =0, j = i+1;
         while(j < n){
-            if(nums[j] != 0 && nums[i] == 0){
+            if(nums[i] == 0 && nums[j]!=0){
                 swap(nums[i], nums[j]);
                 i++;
-                j++;
-                continue;
             } else if(nums[i] != 0){
                 i++;
             }
