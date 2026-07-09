4d2f595aab2f2048f0e4436736e6bbfd34fab9e9
Rituja Sinha <109161939+RitujaSinha@users.noreply.github.com>
2026-07-09 15:35:37 +0530

    Time: 0 ms (100%), Space: 13.3 MB (53.65%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,18 +1,18 @@
 class Solution {
 class DisjointSet{
-public:
-vector<int> parent, rank, size;
+public: 
+    vector<int> parent,size;
 
-DisjointSet(int n){
-    rank.resize(n+1, 0);
-    parent.resize(n+1);
-    size.resize(n+1);
+    DisjointSet(int n){
+        parent.resize(n+1, 0);
+        size.resize(n+1);
 
-    for(int i = 0; i <=n; i++){
-        parent[i] = i;
-        size[i] = 1;
+        for(int i =0; i <=n; i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
     }
-}
+
     int findUPar(int node){
         if(node == parent[node]){
             return node;
@@ -29,7 +29,7 @@ DisjointSet(int n){
         if(size[ulp_u] < size[ulp_v]){
             parent[ulp_u] = ulp_v;
             size[ulp_v] += size[ulp_u];
-        } else {
+        } else{
             parent[ulp_v] = ulp_u;
             size[ulp_u] += size[ulp_v];
         }
@@ -43,8 +43,12 @@ public:
         for(auto it: edges){
             int u = it[0];
             int v = it[1];
-            if(ds.findUPar(u) == ds.findUPar(v)) return {u, v};
-            else ds.unionBySize(u, v);
+
+            if(ds.findUPar(u)== ds.findUPar(v)){
+                return {u, v};
+            } else{
+                ds.unionBySize(u, v);
+            }
         }
         return {};
     }
